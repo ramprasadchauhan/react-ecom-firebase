816f3f10784088320d37997b6bd53d185d75bdc2
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-02-24 20:10:51 +0530

    create navbar with searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Button } from "@material-tailwind/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NoPage from "./pages/NoPage";
